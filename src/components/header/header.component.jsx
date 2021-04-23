c216138b168e46bdaac7257f7bd957d96fe37c96
Sauvic <sauvicpaulchy@gmail.com>
2021-04-23 18:28:46 +0530

    Added checkout page, checkout-item componenet and updated cart to use selectors from  reselect

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
+
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./header.styles.scss";
 
@@ -34,18 +39,8 @@ const Header = ({ currentUser, hidden }) => (
 	</div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-	// advanced destructuring of state is done..
-	currentUser,
-	hidden,
+const mapStateToProps = createStructuredSelector({
+	currentUser: selectCurrentUser,
+	hidden: selectCartHidden,
 });
-// we write a function mapStateToProps (can be anything but it is standard so,)
-// what we return from the function is an object where the name of the property would be the actual prop that we pass in
-// what we get in the function is the state object, this state is the root reducer
-// now here we pass in currentUser property where the value would be state.user.currentUser
-// (user will give userReducer and from there we get the currentUser value  )
-
-export default connect(mapStateToProps)(Header); // The first argument of connect is a function that allows us to access the state
-//  with the state being our  root reducer
-
-// The mapStateToProps and the connect we would use anywhere we need properties from our reducers
+export default connect(mapStateToProps)(Header);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,6 +7,11 @@ export const selectCartItems = createSelector(
 	(cart) => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+	[selectCart],
+	(cart) => cart.hidden
+);
+
 export const selectCartItemsCount = createSelector(
 	[selectCartItems],
 	(cartItems) =>
@@ -15,4 +20,11 @@ export const selectCartItemsCount = createSelector(
 				accumalatedQuantity + cartItems.quantity,
 			0
 		)
+);
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+	cartItems.reduce(
+		(accumalatedQuantity, cartItems) =>
+			accumalatedQuantity + cartItems.quantity * cartItems.price,
+		0
+	)
 );
