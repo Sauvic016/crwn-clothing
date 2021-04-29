068061cbd3a2a1b846702c1786b239ad5721759e
Sauvic <sauvicpaulchy@gmail.com>
2021-04-29 13:32:16 +0530

    adding redux-persist to persist our cart reducer across sessions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux"; //Provider is a component that we get from react-redux
-// This provider is a component class that we want to wrap around the entire application
-// Because we want everything inside to have access to this store object that we get from redux
-import store from "./redux/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./redux/store.js";
 
 import "./index.css";
 import App from "./App";
@@ -13,7 +13,9 @@ import App from "./App";
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
-			<App />
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
 		</BrowserRouter>
 	</Provider>,
 	document.getElementById("root")
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,20 @@
 // our root reducer is an object with a property user which points to userReducer
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
-export default combineReducers({
+
+const persistConfig = {
+	key: "root",
+	storage,
+	whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
