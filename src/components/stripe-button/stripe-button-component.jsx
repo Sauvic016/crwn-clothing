4e43edb8952c3e92577f54167ea9f8d31010c00c
Sauvic <sauvicpaulchy@gmail.com>
2021-05-09 18:06:52 +0530

    updating stripe integration and updating the favicon and title

---
+++ src/components/stripe-button/stripe-button-component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey =
+		"pk_test_51IpB9gSCU19wYyxT0JW9eJ48O5Ws8PdwkmVNNOnHklznB6ztMYkAcvDpfBbHuc13wPVDbOo2jW9QFkp4MJr0Kc8700HtzkVuLn";
+
+	const onToken = (token) => {
+		console.log(token);
+		alert("Payment Successful");
+	};
+
+	return (
+		<StripeCheckout
+			label="Pay Now"
+			name="CRWN Clothing Ltd."
+			billingAddress
+			shippingAddress
+			image="https://svgshare.com/i/CUz.svg"
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel="Pay Now"
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+};
+
+export default StripeCheckoutButton;
